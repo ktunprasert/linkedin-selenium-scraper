861bce23a91d699bf7a161f7c9121f6046223e98
ktunprasert <ktunprasert@outlook.com>
2023-06-10 13:33:36 +0100

    scraper: cleaned up imports

--- scraper.py
+++ scraper.py
@@ -1,13 +1,7 @@
 import time
 import logging
 from selenium import webdriver
-from selenium.webdriver.chrome.service import Service
-from selenium.webdriver.common.keys import Keys
-from selenium.webdriver.common.by import By
-from selenium.webdriver.support.ui import WebDriverWait
-from selenium.webdriver.support import expected_conditions as EC
 from bs4 import BeautifulSoup
-from datetime import datetime
 
 
 class LinkedInScraper:
